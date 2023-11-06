bd584e72239e26be9d7e5d6135610f42559404e0
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2023-11-06 10:20:06 +0100

    Added DB cluster

--- src/main/java/org/nbd/MongołConfig.java
+++ src/main/java/org/nbd/MongołConfig.java
@@ -1,8 +1,83 @@
 package org.nbd;
 
 import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.*;
+import org.bson.UuidRepresentation;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.Conventions;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.nbd.model.TestDBItem;
 
-public class MongołConfig {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongołConfig implements AutoCloseable{
     private static ConnectionString connectionString = new ConnectionString(
             "mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=replica_set_single");
+    private final MongoCredential credential = MongoCredential.createCredential(
+            "admin", "admin", "password".toCharArray());
+
+    private final String collectionName = "testCollection";
+
+    private final CodecRegistry pojoCodecRegistry = CodecRegistries.fromProviders(PojoCodecProvider.builder()
+            .automatic(true)
+            .conventions(List.of(Conventions.ANNOTATION_CONVENTION))
+            .build());
+
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+    public MongołConfig() {
+        // Init DB connection
+        this.initDbConnection();
+
+        // Delete test collection, if it exists
+        MongoIterable<String> list = this.database.listCollectionNames();
+        for (String name : list) {
+            if (name.equals(collectionName)) {
+                this.database.getCollection(name).drop();
+                break;
+            }
+        }
+
+        // Create test collection
+        this.database.createCollection(collectionName);
+    }
+
+    void initDbConnection() {
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .credential(credential)
+                .applyConnectionString(connectionString)
+                .uuidRepresentation(UuidRepresentation.STANDARD)
+                .codecRegistry(CodecRegistries.fromRegistries(
+                        // CodecRegistries.fromProviders(new UniqueIdCodecProvider()),
+                        MongoClientSettings.getDefaultCodecRegistry(),
+                        pojoCodecRegistry
+                ))
+                .build();
+
+        this.mongoClient = MongoClients.create(settings);
+        this.database = mongoClient.getDatabase("admin");
+    }
+
+    public void testAdd(TestDBItem testDBItem) {
+        MongoCollection<TestDBItem> testDBItemMongoCollection =
+                this.database.getCollection(collectionName, TestDBItem.class);
+        testDBItemMongoCollection.insertOne(testDBItem);
+    }
+
+    public ArrayList<TestDBItem> testGet() {
+        MongoCollection<TestDBItem> testDBItemMongoCollection =
+                this.database.getCollection(collectionName, TestDBItem.class);
+        return testDBItemMongoCollection.find().into(new ArrayList<>());
+    }
+
+
+    @Override
+    public void close() {
+        this.mongoClient.close();
+    }
 }
--- src/main/java/org/nbd/ProgramInstance.java
+++ src/main/java/org/nbd/ProgramInstance.java
@@ -1,11 +1,23 @@
 package org.nbd;
 
+import org.nbd.model.TestDBItem;
 import org.nbd.model.TravelAgency;
+
+import java.util.ArrayList;
 
 public class ProgramInstance {
     public static void main(String[] args) {
         TravelAgency sharkTours = new TravelAgency(1000);
         System.out.println("Welcome to Shark Tours!");
+
+        try (MongołConfig mongoRepository = new MongołConfig()) {
+            TestDBItem testDBItem = new TestDBItem("TestName", 420);
+            mongoRepository.testAdd(testDBItem);
+            ArrayList<TestDBItem> testDBItemArrayList = mongoRepository.testGet();
+            System.out.println(testDBItemArrayList.get(0));
+        } catch (Exception e) {
+            System.out.println("oops:\n" + e);
+        }
         // TODO: testing env
     }
 }
