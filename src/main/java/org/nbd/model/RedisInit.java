8af7b3decc0af75a0c8e35f54d1f110bc6dc865a
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2023-12-11 16:02:03 +0100

    Added connectionString in a resource file

--- src/main/java/org/nbd/model/RedisInit.java
+++ src/main/java/org/nbd/model/RedisInit.java
@@ -6,6 +6,10 @@ import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisClientConfig;
 import redis.clients.jedis.JedisPooled;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @Getter
 public class RedisInit {
     private JedisPooled jedisPooled;
@@ -16,8 +20,15 @@ public class RedisInit {
 
     public void initConnection()
     {
-        JedisClientConfig clientConfig = DefaultJedisClientConfig.builder().build();
-        jedisPooled = new JedisPooled(new HostAndPort("localhost", 6379), clientConfig);
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("redis_connection_props")) {
+            Properties props = new Properties();
+            props.load(input);
+
+            JedisClientConfig clientConfig = DefaultJedisClientConfig.builder().build();
+            jedisPooled = new JedisPooled(new HostAndPort(props.getProperty("host"), Integer.parseInt(props.getProperty("port"))), clientConfig);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
