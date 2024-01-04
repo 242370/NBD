acde9a92a0a848ba5d52e42530173f16597458a5
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2024-01-04 14:26:28 +0100

    Deleted managers

--- src/main/java/org/nbd/ProgramInstance.java
+++ src/main/java/org/nbd/ProgramInstance.java
@@ -1,13 +1,11 @@
 package org.nbd;
 
 import org.nbd.model.Accommodation;
-import org.nbd.model.TravelAgency;
 import org.nbd.repos.AccommodationRepo;
 import org.nbd.repos.CassandraManager;
 
 public class ProgramInstance {
     public static void main(String[] args) {
-        TravelAgency sharkTours = new TravelAgency(1000);
         System.out.println("Welcome to Shark Tours!");
 
         try(CassandraManager cassandra = new CassandraManager())
@@ -21,6 +19,7 @@ public class ProgramInstance {
 
             Accommodation newa = repo.getByID(1);
             System.out.println(newa.getDestination());
+            // TODO: application.conf to resources
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
